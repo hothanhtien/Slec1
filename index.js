e199c03aa2e8654aba46aa3f72b5d32fdca1c95e
Ho Thanh Tien <hothanhtienqb@gmail.com>
2024-05-19 23:32:37 +0700

    ok

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const fs = require('fs');
 const app = express()
 const port = 3000
 
@@ -36,6 +37,7 @@ const users = [
 
 
 const logToken = (req, res, next) => {
+  req.user= 'ten'
   const token = req.headers['authorization']
   if (token) {
       console.log('Authorization bearer token',  token)
@@ -46,55 +48,130 @@ const logToken = (req, res, next) => {
   next();
 }
 
-app.get('/users', logToken
+app.get('/users'
+  // ,logToken
   ,(req, res) => {
-    console.log('1. Get tất cả dữ liệu trong mảng')
-    console.log(users)
-    res.send('users')
+    req.user // biết ai đang đăng nhập 
+    fs.readFile('data.json', 'utf8', (err, data) => {
+        if (err) {
+          res.send('lỗi đọc file')
+          return
+      }
+      res.send(data)
+    })
 })
+
+
+
+app.post('/create/:id', (req, res) => {
+  console.log('3. Tạo người dùng mới')
+  console.log('Các trường và value muốn tạo: ', req.body)
+
+  const newUser = { //req.body luôn
+    id: parseInt(req.params.id),
+    name: req.body.name,
+    Age: parseInt(req.body.Age)
+  }
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) {
+      res.send('lỗi đọc file');
+      return;
+    }
+    let dataUsers = JSON.parse(data)
+    console.log(dataUsers)
+    console.log(newUser)
+    const newData = [...dataUsers, newUser]
+    // console.log('hi',newData)
+    fs.writeFile('data.json', JSON.stringify(newData), (err) => {
+      if (err) {
+        res.send('lỗi khi viết file', err)
+        return
+      }
+      res.send('data đã được lưu')
+    })
+  })
+  
+})
+
+app.put('/update/:id', (req, res) => {
+  console.log('4. Update dữ liệu')
+  console.log('Id name Age bạn muốn update ', req.params.id)
+  const name1 = req.body.name;
+  const id1 = parseInt(req.params.id);
+  const age1 = parseInt(req.body.Age)
+  console.log('log ne', {id1, name1, age1})
+  // const userUpdate = users.find(user => user.id === parseInt(req.params.id))
+  // userUpdate.name = req.body.name;
+  // userUpdate.Age = parseInt(req.body.Age);
+  fs.readFile('data.json', 'utf8', (err, data) => {
+      if (err) {
+        res.send('Lỗi đọc file')
+        return
+      }
+      // console.log(JSON.parse(data))
+      const newData = JSON.parse(data).map((item) => {
+        if (item.id == req.params.id)
+          return {
+              id: id1, 
+              name: name1,
+              age: age1
+          }
+        return item
+      })
+      // console.log(newData)
+      // console.log(newData)
+      fs.writeFile('data.json', JSON.stringify(newData), (err) => {
+        if (err) {
+          res.send('Lỗi ghi dữ liệu')
+          return
+        }
+        res.send('data updated')
+      })
+  })
+})
+
+app.delete('/delete/:id', (req, res) => { 
+  console.log('5. Xóa dữ liệu qua id')
+  console.log('Id người nhập muốn xóa' ,req.params.id)
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) {
+      res.send('Lỗi đọc dữ liệu')
+      return
+    }
+    const dataParse = JSON.parse(data);
+    const userIndex = dataParse.findIndex(element => element.id == req.params.id);
+    console.log(userIndex)
+    dataParse.splice(userIndex, 1);
+    console.log(dataParse)
+    // res.send('ok')
+    fs.writeFile('data.json', JSON.stringify(dataParse), (err) => {
+      if (err) {
+        res.send('Lỗi ghi dữ liệu')
+        return
+      }
+      res.send('data updated')
+    })
+  })
+
+})
+
+// app.get('users/nguu', (req, res) => {
+//     return res.send('Vào đây là ngu')
+// })
 
 app.get('/users/:id', (req, res) => {
   console.log('2. Get từng user cụ thể khi truyền params')
   console.log('ID muốn lấy ra thông qua params nè' ,req.params.id)
-  const a = users.filter(user => user.id === parseFloat(req.params.id))
-  console.log('Người dùng có id la: ', req.params.id, 'la:');
-  console.log(a);
-  res.send(a)
-})
-
-app.post('/create', (req, res) => {
-  console.log('3. Tạo người dùng mới')
-  console.log('Các trường và value muốn tạo: ', req.body)
-
-  const newUser = {
-    id: parseInt(req.body.id),
-    name: req.body.name,
-    Age: parseInt(req.body.Age)
-  }
-
-  users.push(newUser)
-  console.log('Danh sach sau khi tao la: ')
-  console.log(users)
-  res.send('Hello World!')
-})
-
-app.put('/update', (req, res) => {
-  console.log('4. Update dữ liệu')
-  console.log('Id name Age bạn muốn update ', req.body)
-  const userUpdate = users.find(user => user.id === parseInt(req.body.id))
-  userUpdate.name = req.body.name;
-  userUpdate.Age = parseInt(req.body.Age);
-  console.log('Sau khi update')
-  console.log(users)
-  res.send('update')
-})
-
-app.delete('/delete', (req, res) => { 
-  console.log('5. Xóa dữ liệu qua id')
-  console.log('Id người nhập muốn xóa' ,req.body)
-  const userDelete = users.filter(user => user.id !== parseInt(req.body.id));
-  console.log('Sau khi xóa id', req.body.id, 'la',userDelete);
-  res.send('delete')
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) {
+      res.send('Lỗi đọc dữ liệu')
+      return
+    }
+    const user = JSON.parse(data).filter(element => element.id == req.params.id);
+    console.log(user)
+    res.send(user)
+  })
+  
 })
 
 app.listen(port, () => { 
